e6b1d08cc7325fe4242be66020a3c3ac481824b7
tourniquetrules <tourniquetrules@gmail.com>
2024-10-25 08:30:03 -0500

    Update gridbox.sh

--- gridbox.sh
+++ gridbox.sh
@@ -2,7 +2,7 @@
 sudo -i &&
 sudo apt update &&
 sudo apt install nano zip curl -y &&
-mkdir -p miners/{trex,lolMiner,ccminer,rigel,wildrig} &&
+mkdir -p miners/{trex,lolMiner,ccminer,rigel,wildrig,xmrig,cpuminer} &&
 wget -P miners/ccminer https://github.com/1NF1N18Y/ccminer-Points/releases/download/v0.1-beta/test-release.zip &&
 unzip test-release.zip &&
 unzip miners/ccminer/test-release.zip -d miners/ccminer &&
@@ -15,4 +15,8 @@ wget -P miners/rigel https://github.com/rigelminer/rigel/releases/download/1.19.
 tar -xzvf miners/rigel/rigel-1.19.1-linux.tar.gz --strip-component=1 -C miners/rigel &&
 wget -P miners/wildrig https://github.com/andru-kun/wildrig-multi/releases/download/0.40.8/wildrig-multi-linux-0.40.8.tar.xz &&
 xz -d miners/wildrig/wildrig-multi-linux-0.40.8.tar.xz &&
-tar -xf miners/wildrig/wildrig-multi-linux-0.40.8.tar -C miners/wildrig
+tar -xf miners/wildrig/wildrig-multi-linux-0.40.8.tar -C miners/wildrig &&
+wget -P miners/xmrig https://github.com/xmrig/xmrig/releases/download/v6.22.1/xmrig-6.22.1-linux-static-x64.tar.gz
+tar -xzvf miners/xmrig/xmrig-6.22.1-linux-static-x64.tar.gz --strip-component=1 -C miners/xmrig &&
+wget -P miners/cpuminer https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.41/cpuminer-opt-linux-5.0.41a.tar.gz &&
+tar -xzvf miners/cpuminer/cpuminer-opt-linux-5.0.41a.tar.gz -C miners/cpuminer
